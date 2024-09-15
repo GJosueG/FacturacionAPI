44a13f3f69520ab0d99a8d88bfb9b7e3e9f5dfbb
BrissVasquez <brizeydavasques776@gmail.com>
2024-09-14 20:13:52 -0600

    Creación de DTOs y servicios para producto.

---
+++ FacturacionAPI/Services/Productos/IProductoServices.cs
@@ -0,0 +1,13 @@
+﻿using FacturacionAPI.DTOs;
+
+namespace FacturacionAPI.Services.Productos
+{
+    public interface IProductoServices
+    {
+        Task<int> PostProducto(ProductoRequest producto);
+        Task<List<ProductoResponse>> GetProductos();
+        Task<ProductoResponse> GetProducto(int productoId);
+        Task<int> PutProducto(int productoId, ProductoRequest producto);
+        Task<int> DeleteProducto(int productoId);
+    }
+}
