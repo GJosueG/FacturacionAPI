6c73da1e4bf049aed19abdcb03df6edfb0fd0c2d
AlexGarci4 <khampoz@gmail.com>
2024-09-24 21:17:46 -0600

    Se agregó llave ronánea de Productos a Factura y Tickets. En DTO y Models

--- FacturacionAPI/Models/Factura.cs
+++ FacturacionAPI/Models/Factura.cs
@@ -11,15 +11,19 @@ public partial class Factura
 
     public decimal Cantidad { get; set; }
 
-    public decimal Precio { get; set; }
+    //public decimal Precio { get; set; }
 
     public decimal Impuesto { get; set; }
 
     public DateTime FechaEmision { get; set; }
 
+    public int? ProductoId { get; set; }
+
     public int? UsuarioId { get; set; }
 
     public int? EstadoId { get; set; }
+
+    public virtual Producto? Producto {  get; set; }
 
     public virtual Estado? Estado { get; set; }
 
--- FacturacionAPI/Models/Ticket.cs
+++ FacturacionAPI/Models/Ticket.cs
@@ -11,14 +11,16 @@ public partial class Ticket
 
     public decimal Cantidad { get; set; }
 
-    public decimal Precio { get; set; }
+    //public decimal Precio { get; set; }
 
     public decimal Total { get; set; }
 
+    public int? ProductoId { get; set; }
     public int? UsuarioId { get; set; }
 
     public int? EstadoId { get; set; }
 
+    public virtual Producto? Producto { get; set; }
     public virtual Estado? Estado { get; set; }
 
     public virtual Usuario? Usuario { get; set; }
